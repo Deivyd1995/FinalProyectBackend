77de35cdc016ad5e387044a4217d6848b334eb1d
cristaldoplf <cristaldoplf@gmail.com>
2022-05-18 06:38:34 -0300

    Agrego funciones para realizar el mapeo entre la entidad Category y CategoryDTO

--- IntegradorBackend/src/main/java/com/dh/grup8/IntegradorBackend/model/service/Impl/CategoryService.java
+++ IntegradorBackend/src/main/java/com/dh/grup8/IntegradorBackend/model/service/Impl/CategoryService.java
@@ -1,4 +1,55 @@
 package com.dh.grup8.IntegradorBackend.model.service.Impl;
 
-public class CategoryService {
+import com.dh.grup8.IntegradorBackend.model.dto.CategoryDTO;
+import com.dh.grup8.IntegradorBackend.model.entity.Category;
+import com.dh.grup8.IntegradorBackend.model.repository.ICategoryRepository;
+import com.dh.grup8.IntegradorBackend.model.service.ICategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class CategoryService implements ICategoryService {
+
+    @Autowired
+    ICategoryRepository categoryRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public CategoryDTO findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public CategoryDTO create(CategoryDTO categoryDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public CategoryDTO update(CategoryDTO categoryDTO) {
+        return null;
+    }
+
+    @Override
+    public Set<CategoryDTO> findAll() {
+        return null;
+    }
+
+    //------ MAPPER ------
+    private CategoryDTO mapToDTO(Category category) {
+        CategoryDTO categoryDTO = modelMapper.map(category, CategoryDTO.class);
+        return categoryDTO;
+    }
+
+    private Category mapToEntity(CategoryDTO categoryDTO) {
+        Category category = modelMapper.map(categoryDTO, Category.class);
+        return category;
+    }
 }
