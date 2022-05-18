7d4be3573931d3334bde6c48b0e47ec4254cce63
cristaldoplf <cristaldoplf@gmail.com>
2022-05-18 06:54:32 -0300

    crud basico de CategoryService terminado

--- IntegradorBackend/src/main/java/com/dh/grup8/IntegradorBackend/model/service/Impl/CategoryService.java
+++ IntegradorBackend/src/main/java/com/dh/grup8/IntegradorBackend/model/service/Impl/CategoryService.java
@@ -7,6 +7,8 @@ import com.dh.grup8.IntegradorBackend.model.service.ICategoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CategoryService implements ICategoryService {
@@ -21,27 +23,40 @@ public class CategoryService implements ICategoryService {
     public CategoryDTO findById(Long id) {
         CategoryDTO response = null;
         Category category = categoryRepository.findById(id).get();
-        return null;
+        if (category != null) {
+            response = mapToDTO(category);
+        }
+        return response;
     }
 
     @Override
     public CategoryDTO create(CategoryDTO categoryDTO) {
-        return null;
+        Category categorySaveResponse = categoryRepository.save(mapToEntity(categoryDTO));
+        CategoryDTO responseCategoryDTO = mapToDTO(categorySaveResponse);
+        return responseCategoryDTO;
     }
 
     @Override
     public void deleteById(Long id) {
-
+        Category category = categoryRepository.findById(id).get();
+        categoryRepository.delete(category);
     }
 
     @Override
     public CategoryDTO update(CategoryDTO categoryDTO) {
-        return null;
+        Category category = mapToEntity(categoryDTO);
+        Category responseCategorySave = categoryRepository.save(category);
+        return mapToDTO(responseCategorySave);
     }
 
     @Override
     public Set<CategoryDTO> findAll() {
-        return null;
+        List<Category> category_list = categoryRepository.findAll();
+        Set<CategoryDTO> categoryDTO_list = new HashSet<>();
+        for (Category cat : category_list) {
+            categoryDTO_list.add(mapToDTO(cat));
+        }
+        return categoryDTO_list;
     }
 
     //------ MAPPER ------
